6f5eb6e95718f991c4ec6c06220e6420c5f85585
Fábio Santos <fabiosantosart@gmail.com>
2016-03-24 16:39:36 +0000

    docs: update example for new props

--- src/example.js
+++ src/example.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill';
 import React from 'react';
-import Storycollection from './';
+import StoryCollection from './';
 /* eslint-disable max-len */
 const exampleStories = [
   {
@@ -8,18 +8,24 @@ const exampleStories = [
     image: 'http://cdn.static-economist.com/sites/default/files/20150516_BLP510_0.jpg',
     source: 'The Economist online',
     webUrl: '/whichmba/financing-embas-youll-pay',
+    itemType: 'https://schema.org/BlogPosting',
+    itemProp: 'blogPost',
   },
   {
     title: 'Keeping it on the company campus',
     image: 'http://cdn.static-economist.com/sites/default/files/20150516_WBD001.jpg',
     source: 'The Economist online',
     webUrl: '/news/business/21651217-more-firms-have-set-up-their-own-corporate-universities-they-have-become-less-willing-pay',
+    itemType: 'https://schema.org/BlogPosting',
+    itemProp: 'blogPost',
   },
   {
     title: 'EMBA supercommuters: Interminable terminals',
     image: 'http://cdn.static-economist.com/sites/default/files/20150516_BLP511_0.jpg',
     source: 'The Economist online',
     webUrl: '/whichmba/emba-supercommuters-interminable-terminals',
+    /* itemType defaults to component-teaser's default itemType, Article */
+    /* itemProp defaults to component-teaser's default itemProp, article */
   },
   {
     title: 'Donald Trump reveals his isolationist foreign-policy instincts',
@@ -31,6 +37,11 @@ const exampleStories = [
 /* eslint-enable max-len */
 
 export default (
-  <Storycollection stories={exampleStories} date="Mar 14th 2016" changed="13.01 GMT" />
+  <StoryCollection
+    stories={exampleStories}
+    date="Mar 14th 2016"
+    changed="13.01 GMT"
+    itemType="https://bib.schema.org/Collection"
+  />
 );
 
